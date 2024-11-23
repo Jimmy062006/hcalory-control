aa0f94d09cd80eabd027cb8bc981285a5a168e4e
Evan Foster <evan.sterling.foster@gmail.com>
2024-11-23 15:29:27 -0700

    Incremental improvements from testing
    I had an incomplete understanding of the heater's state machine which
    was leading to crashses. Similarly, the previous model of connection
    management was really unreliable.

--- hcalory_control/heater.py
+++ hcalory_control/heater.py
@@ -4,11 +4,15 @@ import asyncio
 import dataclasses
 import enum
 import json
+import logging
 from typing import Any
 
 import bleak
 import bleak_retry_connector
 import datastruct
+from bleak import BleakError
+
+logger = logging.getLogger(__name__)
 
 
 class ListableEnum(enum.Enum):
@@ -21,8 +25,8 @@ command_header = bytes.fromhex("000200010001000e040000090000000000000000")
 
 
 class Command(bytes, ListableEnum):
-    start_heat = command_header + bytes.fromhex("010e")
-    stop_heat = command_header + bytes.fromhex("020f")
+    stop_heat = command_header + bytes.fromhex("010e")
+    start_heat = command_header + bytes.fromhex("020f")
     up = command_header + bytes.fromhex("0310")
     down = command_header + bytes.fromhex("0411")
     gear = command_header + bytes.fromhex("0714")
@@ -34,17 +38,30 @@ class Command(bytes, ListableEnum):
 
 
 class HeaterState(int, ListableEnum):
+    """
+    The current state of the heater.
+    This heater has such a weird state machine. Here are the state transitions I've observed:
+    off -> ignition_received -> ignition_starting -> igniting -> heating -> running
+    running -> cooldown_received -> cooldown_starting -> cooldown -> off
+    """
+
     off = 0
     cooldown = 65
-    igniting = 128
+    cooldown_starting = 67
+    cooldown_received = 69
+    ignition_received = 128
+    ignition_starting = 129
+    igniting = 131
     running = 133
-    idle = 135  # THIS IS A WILD GUESS. I have absolutely no clue what this is.
+    heating = 135
+    error = 255
 
 
 class HeaterMode(int, ListableEnum):
     off = 0
     thermostat = 1
     gear = 2
+    ignition_failed = 8
 
 
 @dataclasses.dataclass
@@ -74,6 +91,57 @@ class HeaterResponse(datastruct.DataStruct):
     def ambient_temperature(self) -> int:
         return int(self._ambient_temperature.hex(), 16) // 10
 
+    @property
+    def cooldown(self) -> bool:
+        """
+        Returns whether or not the heater is in its uninterruptible post-stop cooldown stage
+        """
+        # maybe a bit weird to use a case statement here, but it's more concise than doing an if any().
+        match self.heater_state:
+            case (
+                HeaterState.cooldown_received
+                | HeaterState.cooldown_starting
+                | HeaterState.cooldown
+            ):
+                return True
+        return False
+
+    @property
+    def preheating(self) -> bool:
+        """
+        Returns whether or not the heater is in its uninterruptible pre-start ignition and heating stage
+        """
+        match self.heater_state:
+            case (
+                HeaterState.ignition_received
+                | HeaterState.ignition_starting
+                | HeaterState.igniting
+                | HeaterState.heating
+            ):
+                return True
+        return False
+
+    @property
+    def running(self) -> bool:
+        match self.heater_state:
+            case (
+                HeaterState.off
+                | HeaterState.cooldown
+                | HeaterState.cooldown_received
+                | HeaterState.cooldown_starting
+                | HeaterState.error
+            ):
+                return False
+            case (
+                HeaterState.ignition_received
+                | HeaterState.ignition_starting
+                | HeaterState.igniting
+                | HeaterState.running
+                | HeaterState.heating
+            ):
+                return True
+        return False
+
     def asdict(self) -> dict[str, Any]:
         return {
             "heater_state": self.heater_state.name,
@@ -82,48 +150,135 @@ class HeaterResponse(datastruct.DataStruct):
             "voltage": self.voltage,
             "body_temperature": self.body_temperature,
             "ambient_temperature": self.ambient_temperature,
+            "running": self.running,
+            "cooldown": self.cooldown,
+            "preheating": self.preheating,
         }
 
 
 class HCaloryHeater:
     write_characteristic_id = "0000fff2-0000-1000-8000-00805f9b34fb"
     read_characteristic_id = "0000fff1-0000-1000-8000-00805f9b34fb"
-    command_header = bytes.fromhex("000200010001000e040000090000000000000000")
 
-    def __init__(self):
+    def __init__(
+        self,
+        device: bleak.BLEDevice,
+        bluetooth_timeout: float = 30.0,
+        max_bluetooth_retry_attempts: int = 20,
+    ):
+        self.device: bleak.BLEDevice = device
+        self.bluetooth_timeout: float = bluetooth_timeout
+        self.max_bluetooth_retry_attempts = max_bluetooth_retry_attempts
         self._data_pump_queue: asyncio.Queue[bytearray] = asyncio.Queue()
         self._heater_state: HeaterResponse | None = None
         self.heater_response: HeaterResponse | None = None
         self.bleak_client: bleak.BleakClient | None = None
-        self._device: bleak.BLEDevice | None = None
         self._write_characteristic: bleak.BleakGATTCharacteristic | None = None
         self._read_characteristic: bleak.BleakGATTCharacteristic | None = None
         self._intentional_disconnect: bool = False
+        self._reconnect_event: asyncio.Event = asyncio.Event()
+        self._reconnect_event.clear()
+        self._connect_lock = asyncio.Lock()
+        self._command_lock = asyncio.Lock()
+
+    async def start_heat(self):
+        await self.send_command(Command.start_heat)
+
+    async def stop_heat(self):
+        await self.send_command(Command.stop_heat)
+
+    async def change_setting_up(self):
+        await self.send_command(Command.up)
+
+    async def change_setting_down(self):
+        await self.send_command(Command.down)
+
+    async def set_thermostat_mode(self):
+        await self.send_command(Command.thermostat)
+
+    async def set_gear_mode(self):
+        await self.send_command(Command.gear)
+
+    async def _ensure_connection(self, connection_reason: str = "") -> None:
+        if connection_reason:
+            connection_reason = f" Reason: {connection_reason}"
+        if self._connect_lock.locked():
+            logger.debug(
+                "Connection to heater %s already ongoing. You'll have to wait!%s",
+                self.device.address,
+                connection_reason,
+            )
+        if self.bleak_client is not None and self.bleak_client.is_connected:
+            logger.debug(
+                "Already connected to %s, connection is ensured.%s",
+                self.device.address,
+                connection_reason,
+            )
+            return
+        async with self._connect_lock:
+            if self.bleak_client is not None and self.bleak_client.is_connected:
+                logger.debug(
+                    "(Locked) Already connected to %s, connection is ensured.%s",
+                    self.device.address,
+                    connection_reason,
+                )
+                return
+            try:
+                client = await bleak_retry_connector.establish_connection(
+                    bleak.BleakClient,
+                    self.device,
+                    self.device.address,
+                    self.handle_disconnect,
+                    use_services_cache=True,
+                    max_attempts=self.max_bluetooth_retry_attempts,
+                    timeout=self.bluetooth_timeout,
+                )
+                self._intentional_disconnect = False
+                self._reconnect_event.set()
+                self.bleak_client = client
+            except (asyncio.TimeoutError, BleakError):
+                logger.exception(
+                    "(Locked) Failed to connect to heater %s. This will be retried.%s",
+                    self.device.address,
+                    connection_reason,
+                )
+                raise
+            await self.bleak_client.start_notify(
+                self.read_characteristic, self.data_pump_handler
+            )
 
     @property
     def read_characteristic(self) -> bleak.BleakGATTCharacteristic:
         if self._read_characteristic is None:
             assert self.bleak_client is not None
-            self._read_characteristic = self.bleak_client.services.get_characteristic(
+            read_characteristic = self.bleak_client.services.get_characteristic(
                 self.read_characteristic_id
             )
-        assert self._read_characteristic is not None
+            assert read_characteristic is not None
+            self._read_characteristic = read_characteristic
         return self._read_characteristic
 
     @property
     def write_characteristic(self) -> bleak.BleakGATTCharacteristic:
         if self._write_characteristic is None:
             assert self.bleak_client is not None
-            self._write_characteristic = self.bleak_client.services.get_characteristic(
+            write_characteristic = self.bleak_client.services.get_characteristic(
                 self.write_characteristic_id
             )
-        assert self._write_characteristic is not None
+            assert write_characteristic is not None
+            self._write_characteristic = write_characteristic
         return self._write_characteristic
 
     def handle_disconnect(self, _: bleak.BleakClient) -> None:
-        assert self._device is not None
+        self._reconnect_event.clear()
         if not self._intentional_disconnect:
-            asyncio.get_event_loop().create_task(self.connect(self._device))
+            logger.warning(
+                "Encountered unintentional disconnect from %s.", self.device.address
+            )
+        else:
+            logger.debug(
+                "Encountered intentional disconnect from %s.", self.device.address
+            )
 
     async def data_pump_handler(
         self, _: bleak.BleakGATTCharacteristic, data: bytearray
@@ -142,30 +297,32 @@ class HCaloryHeater:
         await self.bleak_client.disconnect()
 
     async def send_command(self, command: Command):
-        assert self.bleak_client is not None
-        await self.bleak_client.write_gatt_char(self.write_characteristic, command)
+        logger.debug(
+            "Sending %s to heater %s",
+            command.name,
+            self.device.address,
+        )
+        async with self._command_lock:
+            await self._ensure_connection(f"Sending command {command.name}")
+            # I wish there was a way to indicate that _ensure_connection is performing this assert.
+            assert self.bleak_client is not None
+            await self.bleak_client.write_gatt_char(self.write_characteristic, command)
+            logger.debug(
+                "Sent %s to characteristic %s of heater %s",
+                command.name,
+                self.write_characteristic,
+                self.device.address,
+            )
 
-    async def connect(self, device: bleak.BLEDevice) -> None:
-        self._intentional_disconnect = False
-        self._device = device
-        self.bleak_client = await bleak_retry_connector.establish_connection(
-            bleak.BleakClient,
-            device,
-            device.address,
-            self.handle_disconnect,
-            use_services_cache=True,
-            max_attempts=20,
-        )
-        await self.bleak_client.start_notify(
-            self.read_characteristic, self.data_pump_handler
-        )
+    async def wait_for_reconnect(self, timeout: float = 30.0) -> None:
+        async with asyncio.timeout(timeout):
+            await self._reconnect_event.wait()
 
 
 async def run_command(command: Command, address: str) -> None:
     device = await bleak.BleakScanner.find_device_by_address(address, timeout=30.0)
     assert device is not None
-    heater = HCaloryHeater()
-    await heater.connect(device)
+    heater = HCaloryHeater(device)
     pre_command_data = await heater.get_data()
     if command == Command.pump_data:
         print(json.dumps(pre_command_data.asdict(), sort_keys=True, indent=4))
